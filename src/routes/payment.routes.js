08d4e9cd5a14fc2c50a1eb0738b4c4d1a3fb6d33
ali omar mohamed <113925886+aliomarmohamed@users.noreply.github.com>
2026-07-28 03:12:24 +0300

    Delete src/routes/payment.routes.js

--- src/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/payment.controller');
-
-router.post('/checkout', paymentController.createCheckoutSession);
-router.post('/webhook', express.raw({ type: 'application/json' }), paymentController.handleStripeWebhook);
-
-module.exports = router;
